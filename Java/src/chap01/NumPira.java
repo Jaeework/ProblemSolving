4613847e7b2025af3a9304ed5c3c9efdde8cda40
soizzypizzy <soizzypizzy@gmail.com>
2021-06-07 23:46:38 +0900

    Algorithm Practice

--- Java/src/chap01/NumPira.java
+++ Java/src/chap01/NumPira.java
@@ -17,7 +17,7 @@ public class NumPira {
 	
 	static void npira(int n) {
 		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j < n-i+1; j++) {
+			for(int j = 1; j <= n-i; j++) {
 				System.out.print(' ');
 			}
 			for(int j = 1; j <= (i-1)*2+1; j++) {
