05bec57f123b7243861bf0ad63ca8f0bc8ed3283
soizzypizzy <soizzypizzy@gmail.com>
2021-06-19 22:48:42 +0900

    Algorithm Search Practice

---
+++ Java/src/chap03/SeqSearch.java
@@ -0,0 +1,44 @@
+package chap03;
+import java.util.Scanner;
+// 선형 검색
+
+public class SeqSearch {
+	//요솟수가 n인 배열 a에서 key와 같은 요소를 선형 검색합니다.
+	static int seqSearch(int[] a, int n, int key) {
+		int i = 0;
+		
+		while(true) {
+			if(i == n)
+				return -1;	// 검색 실패!
+			if(a[i] == key)
+				return i;	// 검색 성공(인덱스를 반환)
+			
+			i++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("요솟수 : ");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];		// 요솟수가 num인 배열
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ");
+		int key = stdIn.nextInt();
+		int idx = seqSearch(x, num, key);	// 배열 x에서 키 값이 ky인 요소를 검색
+		
+		if(idx == -1)
+			System.out.println("그 값의 요소가 없습니다.");
+		else 
+			System.out.println(key+"은(는) x[" + idx + "]에 있습니다.");
+		
+		
+		
+	}
+}
