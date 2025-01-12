c93f27f39c0e460cb8c857cef972661b424c8b57
Jaeework <jaeework@gmail.com>
2025-01-13 01:48:01 +0900

    Create P276013.sql
    Programmers 276013 : SELECT
    #mysql

---
+++ SQL/Programmers/P276013.sql
@@ -0,0 +1,7 @@
+-- Programmers 276013 : SELECT 
+select id, email, first_name, last_name
+from developer_infos
+where skill_1 like 'Python' 
+    or skill_2 like 'Python' 
+    or skill_3 like 'Python'
+order by id asc
